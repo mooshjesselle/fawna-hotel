03fb6f5906215ef8684b47fe7afaf82b9970e8e7
mooshjesselle <jhunevincentaquinoroces@gmail.com>
2025-10-08 14:55:37 +0800

    Test GitHub linking with app.py change

--- app.py
+++ app.py
@@ -56,4 +56,4 @@ app = create_app()
 from blueprints.routes import *
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=4000, debug=True)
