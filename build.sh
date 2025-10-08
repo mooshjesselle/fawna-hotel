98e3a652b213856c2dd1cd01c9b7202031f78f05
mooshjesselle <jhunevincentaquinoroces@gmail.com>
2025-10-08 20:01:16 +0800

    Render deployment ready

--- build.sh
+++ build.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
-# Install system packages needed by WeasyPrint
+# Update system packages
 apt-get update
+
+# Install dependencies for WeasyPrint and Pillow
 apt-get install -y libffi-dev libjpeg-dev zlib1g-dev libcairo2 libpango-1.0-0 libgdk-pixbuf2.0-0
-# Upgrade pip & setuptools
+
+# Upgrade pip, setuptools, and wheel
 pip install --upgrade pip setuptools wheel
-# Install Python packages
+
+# Install Python dependencies
 pip install -r requirements.txt
