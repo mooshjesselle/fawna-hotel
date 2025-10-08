45f652160be878d30df5694f37989447a725e7f0
mooshjesselle <jhunevincentaquinoroces@gmail.com>
2025-10-08 14:03:36 +0800

    Initial Flask app upload

--- migrations/add_id_verification.py
+++
@@ -1,7 +0,0 @@
-from utils.extensions import db
- 
-def upgrade():
-    # Add new columns to user table
-    with db.engine.connect() as conn:
-        conn.execute('ALTER TABLE user ADD COLUMN id_picture VARCHAR(255)')
-        conn.execute("ALTER TABLE user ADD COLUMN registration_status VARCHAR(20) DEFAULT 'pending'") 
