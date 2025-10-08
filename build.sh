4745cd8a09f37217d401bae44e1a35c72370c904
mooshjesselle <jhunevincentaquinoroces@gmail.com>
2025-10-08 20:15:03 +0800

    Fix Render build with Python 3.12

--- build.sh
+++ build.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-# build.sh
 sudo apt-get update
-sudo apt-get install -y libjpeg-dev zlib1g-dev libpangocairo-1.0-0 libpango1.0-dev libcairo2 libcairo2-dev
+sudo apt-get install -y libjpeg-dev zlib1g-dev libcairo2 libcairo2-dev
 pip install --upgrade pip setuptools wheel
 pip install -r requirements.txt
