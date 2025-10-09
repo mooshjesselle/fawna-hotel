c6762ead0a92fa30c1558b2ca5ab5b2748697bb2
mooshjesselle <jhunevincentaquinoroces@gmail.com>
2025-10-09 21:44:52 +0800

    #

--- config.py
+++ config.py
@@ -9,7 +9,6 @@ class Config:
     MAIL_SERVER = 'smtp.gmail.com'
     MAIL_PORT = 587
     MAIL_USE_TLS = True
-    MAIL_SUPPRESS_SEND = False
     MAIL_USERNAME = os.getenv('MAIL_USERNAME')
     MAIL_PASSWORD = os.getenv('MAIL_PASSWORD')
     MAIL_DEFAULT_SENDER = os.getenv('MAIL_USERNAME')
