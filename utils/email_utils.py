961e2899cdd0189201771f6fb1e959477e8fcd5c
mooshjesselle <jhunevincentaquinoroces@gmail.com>
2025-10-09 23:59:36 +0800

    #

--- utils/email_utils.py
+++ utils/email_utils.py
@@ -54,7 +54,12 @@ def send_email_via_sendgrid(to_email, subject, html_body):
                 {"type": "text/plain", "value": "This email contains HTML content. If you see this, please enable HTML emails."},
                 {"type": "text/html", "value": html_body}
             ],
-            "mail_settings": {"sandbox_mode": {"enable": False}}
+            "mail_settings": {"sandbox_mode": {"enable": False}},
+            "tracking_settings": {
+                "click_tracking": {"enable": False, "enable_text": False},
+                "open_tracking": {"enable": False}
+            },
+            "categories": ["transactional", "otp"]
         }
         headers = {
             "Authorization": f"Bearer {api_key}",
